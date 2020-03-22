f9f7f5597d09a0b0fde1f7fd24f391e51e8fd34e
djevanclark <djevanclark@Evans-MacBook-Pro-2.local>
2020-03-22 15:08:55 -0400

    Added fix

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,12 +8,12 @@ echo "Restarting postgres DB"
 pg_ctlcluster 10 main restart
 
 echo "Setting Up postgres"
-cmd="'psql --command GRANT CONNECT ON airtime to $POSTGRES_USER'"  
-su -s /bin/bash -c $cmd postgres
+cmd="'psql --command CREATE USER $POSTGRES_USER WITH SUPERUSER PASSWORD '"$POSTGRES_PASSWORD"';'"
+sudo -u postgres $cmd 
 cmd="'createdb -O $POSTGRES_USER airtime'"
-su -s /bin/bash -c $cmd postgres
+su -c $cmd postgres
 cmd="'psql --command GRANT CONNECT ON airtime to $POSTGRES_USER'"
-su -s /bin/bash -c $cmd postgres
+su -c $cmd postgres
 exit
 
 #Setup RabbitMQ
